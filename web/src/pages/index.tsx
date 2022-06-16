6bd6284c5ab1355e190d6b08e52246d84813ad4d
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-15 23:09:11 -0300

    feat(web): adding auth0 authorization

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,15 @@
+import { useUser } from '@auth0/nextjs-auth0';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
+  const { user } = useUser();
+  console.log(user);
+
   return (
     <div>
       <h1>Hello World</h1>
+
+      <a href="/api/auth/login">Login</a>
     </div>
   );
 };
