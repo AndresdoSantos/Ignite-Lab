1b0fed26a1050356d68f68b319088657781e64fe
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-15 23:42:11 -0300

    feat(web/purchases): auth0 template

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,4 +1,4 @@
-import { useUser } from '@auth0/nextjs-auth0';
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default function App() {
   const { user } = useUser();
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div>
       <h1>{user?.name || 'Unauthorized'}</h1>
+
+      <a href="api/auth/logout">logout</a>
     </div>
   );
 }
+
+export const getServerSideProps = withPageAuthRequired();
